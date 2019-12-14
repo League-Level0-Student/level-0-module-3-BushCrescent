afe36ec6128c4645370be6d896601ca23adfc665
League Student <league@iMac-24.attlocal.net>
2019-12-14 12:57:28 -0800

    module 3

--- src/_07_years_alive/years_alive.java
+++ src/_07_years_alive/years_alive.java
@@ -1,5 +1,9 @@
 package _07_years_alive;
 
-public class years_alive {
+import javax.swing.JOptionPane;
 
+public class years_alive {
+	public static void main(String[] args) {
+for (int i=0 ; i<14 ; i++)
+	}
 }
